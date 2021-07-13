49373e0ceed980ff9fd1a49fe3da7f5e45c9d1d7
Tristan Gruener <tristan.gruener@gmail.com>
2021-07-13 14:13:33 -0700

    progress

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,32 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <>
+    <header>
+
+        {/* <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1> */}
+
+    </header>
+    <div class="container" id="header-content">
+      <span id="name">
+        <h3>Marianne B. Rowe, MS</h3>
+        <h4>Marriage & Family Therapist</h4>
+
+        <div id="summary">Psychotherapy, Ecotherapy, and Mindfulness Meditation Instruction</div>
+
+      </span>
     </div>
-  </header>
+  </>
 )
 
 Header.propTypes = {
